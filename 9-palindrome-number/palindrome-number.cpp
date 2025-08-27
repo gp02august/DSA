53d84d6abbdebd991a4ef6e19e126553598748e9
Gyanendra Pal <125917025+gp02august@users.noreply.github.com>
2025-08-27 18:26:07 +0530

    Time: 6 ms (17.84%) | Memory: 11.1 MB (5.06%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -5,20 +5,20 @@ public:
         if(x<0)
             return false;
 
-        int temp = x; long sum =0;
-        while(x != 0){
-            int rem;
-            rem = x%10;
-            sum =sum*10+rem;
-            x /=10;
-        }
-        return sum==temp;
+        // int temp = x; long sum =0;
+        // while(x != 0){
+        //     int rem;
+        //     rem = x%10;
+        //     sum =sum*10+rem;
+        //     x /=10;
+        // }
+        // return sum==temp;
 
-    //     string res = to_string(x);
-    //    string temp =  res;
-    //    reverse(temp.begin(), temp.end());
-    //    int x1=stoi(temp);
+        string res = to_string(x);
+       string temp =  res;
+       reverse(temp.begin(), temp.end());
+       long x1=stol(temp);
 
-    //    return x== x1;
+       return x== x1;
     }
 };
