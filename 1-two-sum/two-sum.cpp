5b003a95dd9e3272a3e81f630136a57d25f65b44
Gyanendra Pal <125917025+gp02august@users.noreply.github.com>
2025-11-22 18:44:03 +0530

    Time: 129 ms (5.40%) | Memory: 14 MB (93.20%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-// Gyanendra
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>res_Vec;
-        /*for(int i=0; i<nums.size(); i++){  // this is the brutforce approach...
-            for(int j=i+1; j<nums.size(); j++){
-                if((nums[i]+nums[j])==target){
-                    res_Vec.push_back(i);
-                    res_Vec.push_back(j);
+        vector<int>ans;
+        int n = nums.size();
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                if((nums[i]+nums[j])==target)
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
                 }
             }
-        }*/
-        // optimize solution......
-         unordered_map<int, int>mp;
-        for(int i=0; i<nums.size(); i++){
-            int complement = target-nums[i];
-            if(mp.find(complement) != mp.end()){
-                res_Vec.push_back(mp[complement]);
-                res_Vec.push_back(i);
-            }
-            mp[nums[i]]=i;
         }
-        return res_Vec;
+        return ans;
     }
 };
