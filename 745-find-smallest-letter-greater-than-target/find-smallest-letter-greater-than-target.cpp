8cfe39725ae5201a28ee7e8043e0af915745b04d
Gyanendra Pal <125917025+gp02august@users.noreply.github.com>
2026-01-31 12:59:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (24.67%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        for(int i=0; i<letters.size(); i++){
+        /*for(int i=0; i<letters.size(); i++){
             if(letters[i]>target)
             {
                 return letters[i];
             }
-            // else{
-            //     return letters[0];
-            // }
         }
-        return letters[0];
+        return letters[0];*/ //using this logic we solve in time complexity of O(n);
+
+        //Now here we can solve this question using binary search algorithm....
+
+        int left=0; int end=letters.size()-1;
+        while(left<=end){
+            int mid = left+(end-left)/2;
+            if(letters[mid]<=target){
+                left=mid+1;
+            }
+            else{
+                end =mid-1;
+            }
+        }
+        if(left == letters.size())
+        {
+            return letters[0];
+        }
+        return letters[left];
     }
 };
