1c186c66fb7691e4b6d0a28de095f82b81fa3c9b
Gyanendra Pal <125917025+gp02august@users.noreply.github.com>
2026-07-30 21:50:43 +0530

    Time: 1174 ms (5.13%) | Memory: 11.3 MB (10.24%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,26 +1,57 @@
 class Solution {
 public:
-    int gcd(int even_sum, int odd_sum){
-        int ans=0;
-        if(odd_sum>even_sum){
-            swap(even_sum, odd_sum);
+    int solve(int even_sum, int odd_sum){
+        vector<int>v; vector<int>vc;
+        for(int i=1; i<=even_sum; i++){
+            if(even_sum%i==0){
+                v.push_back(i);
+
+                if(even_sum/i != i){
+                    v.push_back(even_sum/i);
+                }
+            }
         }
-        while(odd_sum != 0){
-            int rem = even_sum % odd_sum;
-            even_sum = odd_sum;
-            odd_sum = rem;
+        for(int i=1; i<=odd_sum; i++){
+            if(odd_sum%i==0){
+                vc.push_back(i);
+
+                if(odd_sum/i != i){
+                    vc.push_back(odd_sum/i);
+                }
+            }
         }
-        return even_sum;
+        sort(v.begin(), v.end());
+        sort(vc.begin(), vc.end());
+        vector<int>s;
+        int i=0, j=0;
+        while(i<v.size()-1 && j<vc.size()-1){
+            if(v[i]==vc[j])
+            {
+                s.push_back(v[i]);
+                i++; j++;
+            }
+            else if(v[i]>vc[j]){
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        int fact=INT_MIN;
+        for(int k=0; k<s.size(); k++){
+            fact = max(fact, s[k]);
+        }
+        return fact;
     }
     int gcdOfOddEvenSums(int n) {
-        int even_sum=0; int odd_sum=0;
-        for(int i=1; i<=n; i++){
-             odd_sum += (2 * i - 1); // for odd...
+        if(n==1)
+            return 1;
+            
+        int even_sum=0, odd_sum=0;
+        for(int i=0; i<n;i++){
+            even_sum +=2*i+2;
+            odd_sum +=2*i+1;
         }
-        for(int i=1; i<=n; i++){
-            even_sum +=2*i;
-        }
-        int res = gcd(even_sum, odd_sum);
-        return res;
+        return solve(even_sum, odd_sum);
     }
 };
